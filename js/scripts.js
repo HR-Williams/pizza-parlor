3391173a554695970faa8de287ef648d522a48f8
HR Williams <halrubinwilliams@gmail.com>
2021-01-29 16:37:22 -0800

    Remove console.logs

--- js/scripts.js
+++ js/scripts.js
@@ -14,13 +14,9 @@ $(document).ready(function() {
   $("form#pizzaForm").submit(function(e) {
     e.preventDefault();
     let size = parseInt($("#size").val());
-    console.log(size);
     let toppingOne = parseInt($("#toppingOne").val());
-    console.log(toppingOne);
     let toppingTwo = parseInt($("#toppingTwo").val());
-    console.log(toppingTwo);
     let totalPrice = new Pizza(size, toppingOne, toppingTwo);
-    console.log(totalPrice)
     $("#show-price").show();
     $("#price").text(totalPrice.totalCost());
     
