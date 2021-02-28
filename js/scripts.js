32e27ebbec107d93b468230f32cf921b6b075124
HR Williams <halrubinwilliams@gmail.com>
2021-02-27 21:03:34 -0800

    Comment out main branch scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 function Pizza(size, toppings) {
   this.size = size;
-  this.toppings= [];
+  this.toppings= [];  
 }
 
-Pizza.prototype.totalCost = function () {
-  let total = (this.size + this.toppingOne + this.toppingTwo + this.toppingThree)
-  return total
-}
+// Pizza.prototype.totalCost = function () {
+//   let total = (this.size + this.toppingOne + this.toppingTwo + this.toppingThree)
+//   return total
+// }
 
 
-$(document).ready(function() {
-  $("form#pizzaForm").submit(function(e) {
-    e.preventDefault();
-    let size = parseInt($("#size").val());
-    let toppingOne = parseInt($("#toppingOne").val());
-    let toppingTwo = parseInt($("#toppingTwo").val());
-    let toppingThree = parseInt($("#toppingThree").val());
-    let totalPrice = new Pizza(size, toppingOne, toppingTwo, toppingThree);
-    $("#show-price").show();
-    $("#price").text(totalPrice.totalCost());
+// $(document).ready(function() {
+//   $("form#pizzaForm").submit(function(e) {
+//     e.preventDefault();
+//     let size = parseInt($("#size").val());
+//     let toppingOne = parseInt($("#toppingOne").val());
+//     let toppingTwo = parseInt($("#toppingTwo").val());
+//     let toppingThree = parseInt($("#toppingThree").val());
+//     let totalPrice = new Pizza(size, toppingOne, toppingTwo, toppingThree);
+//     $("#show-price").show();
+//     $("#price").text(totalPrice.totalCost());
     
-  });
-});
+//   });
+// });
